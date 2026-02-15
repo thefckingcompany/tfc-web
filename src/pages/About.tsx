f25d801dcd7162b079f23fee18cc7e463e6af067
XI.I.MMV.P <xi.i.mmv.p@MacBook-Air-de-XIIMMVP.local>
2026-02-15 14:18:30 +0000

    Standardize About page top padding to match other pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import { AboutSection } from '../components/home/AboutSection';
 
 const About = () => {
     return (
-        <div className="pt-20 animate-fade-in">
+        <div className="pt-12 animate-fade-in">
             {/* Added pt-20 to account for fixed header if any, or just spacing */}
             <AboutSection />
         </div>
